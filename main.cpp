fb326cb4353d6b8c61739e305674a5ad5a821cef
rabilint <rabilint2007@gmail.com>
2024-10-27 23:54:08 +0200

    add Discriminant tested

--- main.cpp
+++ main.cpp
@@ -32,13 +32,35 @@ int main() {
     }
     a = 1;
     b = 1;
-    if (a & b) {
-        printf("\na == true, b == true" );
-    }
-    b=0;
-    if (a | b) {
-        printf("\na == true, b == false" );
-    }
+
+
+    int binaryOne = 125;
+    printf("\n binaryOne = %b", binaryOne );
+    printf("\n binaryOne = %b", binaryOne << 2);
+    printf("\n binaryOne = %b", binaryOne >> 0);
+
+    int intager = 123;
+    printf("\n%b\n%b", intager,binaryOne  );
+    printf("\n%b", intager & binaryOne );
+    printf("\n%b", intager | binaryOne );
+    printf("\n%b", ~ binaryOne );
+    printf("\n%b", intager ^ binaryOne );
+    int g = intager ^ binaryOne;
+    printf("\ng = %b", g);
+    int h = g^binaryOne;
+    printf("\ng = %b", h);
+
+    //  task 3
+    //  Написати програму з введенням числа у змінну і виведенням її адреси та
+    //  значення через вказівник.
+
+    int usernum;
+    int *point;
+    printf("\n Usernum : ");
+    scanf("%d",&usernum);
+    point = &usernum;
+    printf("%d %p",*point,&point);
+
 
     // task num 4
     //Написати програму розв’язку квадратного рівняння, коефіцієнти якого
@@ -46,17 +68,13 @@ int main() {
 
 
 
-    int x=1,y=1,z=1;
+    int x = 1,y = 1,z = 1;
     printf("\n Enter 3 numbers: ");
     scanf("%d %d %d", &x,&y,&z);
-    int D = pow(y,2) - 4*x*z;
-    // printf("\n D = %d", D);
-    int output = (-y + sqrt(D))/2*x;
-    // printf("\n %d", -y);
-    // printf("%d",sqrt(D));
-    // printf("\n %d", 2*x);
+    int D = pow(y,2) - 4 * x * z;
+    int output = (-y + sqrt(D)) / 2 * x;
     printf("\n Output x2= %d", output);
-    output = (-y - sqrt(D))/2*x;
+    output = (-y - sqrt(D)) / 2 * x;
     printf("\n Output x2= %d", output);
 
     return 0;
