0523399d8ed404e32787843ce1a140dc27943a11
rabilint <rabilint2007@gmail.com>
2024-10-31 12:53:44 +0200

    end of lab5

--- lab5/cmake-build-debug/CMakeFiles/lab5.dir/main.cpp.o.ddi.i
+++ lab5/cmake-build-debug/CMakeFiles/lab5.dir/main.cpp.o.ddi.i
@@ -26377,7 +26377,50 @@ namespace std
 
 # 6 "/home/rabilint/CLionProjects/LNU-lection/lab5/main.cpp"
 int main() {
-# 56 "/home/rabilint/CLionProjects/LNU-lection/lab5/main.cpp"
+
+
+
+
+
+    int userInputNum1, userInputNum2;
+    printf("Please enter two numbers: ");
+    scanf("%d %d", &userInputNum1, &userInputNum2);
+    userInputNum1 > userInputNum2 ? printf("Перше число більше другого") : (userInputNum1 < userInputNum2 ? printf("Друге число більше першого") : printf("Числа рівні"));
+
+
+
+
+
+
+
+    int hypotenusa,katet1,katet2;
+    printf("\nPlease enter lenght of c: ");
+    scanf("%d", &hypotenusa);
+    printf("\nPlease enter lenght of a: ");
+    scanf("%d", &katet1);
+    printf("\nPlease enter lenght of b: ");
+    scanf("%d", &katet2);
+    if (katet1 == katet2 && katet2 == hypotenusa) {
+        printf("трикутник рівносторонній");
+    }
+    else if (katet1 == katet2 || katet2 == hypotenusa || hypotenusa == katet1) {
+        printf("трикутник рівнобедрений");
+    }
+    else if (katet1!= katet2 != hypotenusa) {
+        printf("трикутник різносторонній");
+    }
+
+
+    double katet1vKvadrati = katet1 * katet1, katet2vKvadrati = katet2 * katet2, hypotenusa2vKvadrati = hypotenusa * hypotenusa;
+
+    if (fabs(katet1vKvadrati + katet2vKvadrati - hypotenusa2vKvadrati) < 1e-6 || fabs(katet1vKvadrati + hypotenusa2vKvadrati - katet2vKvadrati) < 1e-6 || fabs(katet2vKvadrati + hypotenusa2vKvadrati - katet1vKvadrati) < 1e-6) {
+        printf("\nТрикутник є прямокутним.\n");
+    } else if ((katet1vKvadrati + katet2vKvadrati > hypotenusa2vKvadrati) && (katet1vKvadrati + hypotenusa2vKvadrati > katet2vKvadrati) && (katet2vKvadrati + hypotenusa2vKvadrati > katet1vKvadrati)) {
+        printf("\nТрикутник є гострокутним.\n");
+    } else {
+        printf("\nТрикутник є тупокутним.\n");
+    }
+# 58 "/home/rabilint/CLionProjects/LNU-lection/lab5/main.cpp"
     char *hundreds[] = {"", "Сто", "Двісті", "Триста", "Чотириста", "П'ятсот", "Шістсот", "Сімсот", "Вісімсот", "Дев'ятсот"};
     char *tens[] = {"", "", "Двадцять", "Тридцять", "Сорок", "П'ятдесят", "Шістдесят", "Сімдесят", "Вісімдесят", "Дев'яносто"};
     char *teens[] = {"Десять", "Одинадцять", "Дванадцять", "Тринадцять", "Чотирнадцять", "П'ятнадцять", "Шістнадцять", "Сімнадцять", "Вісімнадцять", "Дев'ятнадцять"};
--- lab5/main.cpp
+++ lab5/main.cpp
@@ -9,44 +9,44 @@ int main() {
     // // Написати програму, котра видаватиме результат порівняння двох введених з клавіатури
     // // чисел з допомогою оператора «?».
     //
-    // int userInputNum1, userInputNum2;
-    // printf("Please enter two numbers: ");
-    // scanf("%d %d", &userInputNum1, &userInputNum2);
-    // userInputNum1 > userInputNum2 ? printf("Перше число більше другого") : (userInputNum1 < userInputNum2 ? printf("Друге число більше першого") : printf("Числа рівні"));
-    //
-    //
-    // //Task 3
-    // //     Написати програму, котра дозволяє ввести з консолі довжини трьох сторін трикутника
-    // // (наприклад, 3 4 5) і визначити його тип: а) рівносторонній (правильний),
-    // // рівнобедренний, різносторонній; б) прямо-, гостро-, тупо-кутний.
-    //
-    // int hypotenusa,katet1,katet2;
-    // printf("\nPlease enter lenght of c: ");
-    // scanf("%d", &hypotenusa);
-    // printf("\nPlease enter lenght of a: ");
-    // scanf("%d", &katet1);
-    // printf("\nPlease enter lenght of b: ");
-    // scanf("%d", &katet2);
-    // if (katet1 == katet2 && katet2 == hypotenusa) {
-    //     printf("трикутник рівносторонній");
-    // }
-    // else if (katet1 == katet2 || katet2 == hypotenusa || hypotenusa == katet1) {
-    //     printf("трикутник рівнобедрений");
-    // }
-    // else if (katet1!= katet2 != hypotenusa) {
-    //     printf("трикутник різносторонній");
-    // }
-    //
-    //
-    // double katet1vKvadrati = katet1 * katet1, katet2vKvadrati = katet2 * katet2, hypotenusa2vKvadrati = hypotenusa * hypotenusa;
-    //
-    // if (fabs(katet1vKvadrati + katet2vKvadrati - hypotenusa2vKvadrati) < 1e-6 || fabs(katet1vKvadrati + hypotenusa2vKvadrati - katet2vKvadrati) < 1e-6 || fabs(katet2vKvadrati + hypotenusa2vKvadrati - katet1vKvadrati) < 1e-6) {  // 1e-6 Похибка для порівняння чисел із плаваючою точкою
-    //     printf("\nТрикутник є прямокутним.\n");
-    // } else if ((katet1vKvadrati + katet2vKvadrati > hypotenusa2vKvadrati) && (katet1vKvadrati + hypotenusa2vKvadrati > katet2vKvadrati) && (katet2vKvadrati + hypotenusa2vKvadrati > katet1vKvadrati)) {
-    //     printf("\nТрикутник є гострокутним.\n");
-    // } else {
-    //     printf("\nТрикутник є тупокутним.\n");
-    // }
+    int userInputNum1, userInputNum2;
+    printf("Please enter two numbers: ");
+    scanf("%d %d", &userInputNum1, &userInputNum2);
+    userInputNum1 > userInputNum2 ? printf("Перше число більше другого") : (userInputNum1 < userInputNum2 ? printf("Друге число більше першого") : printf("Числа рівні"));
+
+
+    //Task 3
+    //     Написати програму, котра дозволяє ввести з консолі довжини трьох сторін трикутника
+    // (наприклад, 3 4 5) і визначити його тип: а) рівносторонній (правильний),
+    // рівнобедренний, різносторонній; б) прямо-, гостро-, тупо-кутний.
+
+    int hypotenusa,katet1,katet2;
+    printf("\nPlease enter lenght of c: ");
+    scanf("%d", &hypotenusa);
+    printf("\nPlease enter lenght of a: ");
+    scanf("%d", &katet1);
+    printf("\nPlease enter lenght of b: ");
+    scanf("%d", &katet2);
+    if (katet1 == katet2 && katet2 == hypotenusa) {
+        printf("трикутник рівносторонній");
+    }
+    else if (katet1 == katet2 || katet2 == hypotenusa || hypotenusa == katet1) {
+        printf("трикутник рівнобедрений");
+    }
+    else if (katet1!= katet2 != hypotenusa) {
+        printf("трикутник різносторонній");
+    }
+
+
+    double katet1vKvadrati = katet1 * katet1, katet2vKvadrati = katet2 * katet2, hypotenusa2vKvadrati = hypotenusa * hypotenusa;
+
+    if (fabs(katet1vKvadrati + katet2vKvadrati - hypotenusa2vKvadrati) < 1e-6 || fabs(katet1vKvadrati + hypotenusa2vKvadrati - katet2vKvadrati) < 1e-6 || fabs(katet2vKvadrati + hypotenusa2vKvadrati - katet1vKvadrati) < 1e-6) {  // 1e-6 Похибка для порівняння чисел із плаваючою точкою
+        printf("\nТрикутник є прямокутним.\n");
+    } else if ((katet1vKvadrati + katet2vKvadrati > hypotenusa2vKvadrati) && (katet1vKvadrati + hypotenusa2vKvadrati > katet2vKvadrati) && (katet2vKvadrati + hypotenusa2vKvadrati > katet1vKvadrati)) {
+        printf("\nТрикутник є гострокутним.\n");
+    } else {
+        printf("\nТрикутник є тупокутним.\n");
+    }
 
     //Task4
     //Написати програму, котра дозволяє ввести з консолі тризначне число і вивести його
